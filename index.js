d02625f0d80f15d60e66c0ece8fe607f7228fb61
Your Name Main <you@example.com>
2023-03-08 08:55:25 -0800

    fs url added

--- index.js
+++ index.js
@@ -6,29 +6,71 @@ const other = require('./other.js')
 // console.log(result);
 
 const http =require('http')
+const url = require('url')
+const fs = require('fs')
 
-const server = http.createServer((req,res)=>{
+// const server = http.createServer((req,res)=>{
     
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-type' : 'application/json'})
-        res.write(JSON.stringify("Mission ACC Node Mongo Course"))
-        res.end()
-    }
+//     if(req.url === '/'){
+//         res.writeHead(200, {'Content-type' : 'application/json'})
+//         res.write(JSON.stringify("Mission ACC Node Mongo Course"))
+//         res.end()
+//     }
 
-    else if(req.url === '/contact'){
-        res.writeHead(200, {'Content-type' : 'text/html'})
-        res.write('<p>This is contact page</p>')
-        res.end()
-    }
-    if(req.url === '/about'){
-        res.writeHead(200, {'Content-type' : 'text/html'})
-        res.write('<p>This is about page</p>')
-        res.end()
-    }
+//     else if(req.url === '/contact'){
+//         res.writeHead(200, {'Content-type' : 'text/html'})
+//         res.write('<p>This is contact page</p>')
+//         res.end()
+//     }
+//     if(req.url === '/about'){
+//         res.writeHead(200, {'Content-type' : 'text/html'})
+//         res.write('<p>This is about page</p>')
+//         res.end()
+//     }
 
-    console.log(req.url);
-    // res.end("hello node js")
+//     console.log(req.url);
+//     // res.end("hello node js")
+// })
+
+// const server = http.createServer((req,res)=>{
+//     const address_url = "http://localhost:5000/contact?name=bappycountery=bangladesh"
+//     const parse_url = url.parse(address_url,true)
+//     // console.log(parse_url);
+//     const queryObject =parse_url.query
+//     console.log(queryObject);
+
+// })
+
+const server =http.createServer((req,res)=>{
+    if(req.url = '/'){
+        // fs.readFile('data.txt',(err,data)=>{
+        //     // if(err){
+        //     //     res.write('Failed To Read Data')
+        //     // }
+        //     // else{
+        //     //     res.write(data)
+        //     //     res.end()
+        //     // }
+           
+
+        // })
+
+        // const data = fs.readFileSync('data.txt')
+        // res.write(data)
+        // res.end()
+
+        fs.writeFile('newData.txt', 'hello node js !!!',(err)=>{
+            if(err){
+                res.write('Data Failed to write')
+                res.end()
+            }else{
+                res.write('data written successfully')
+                res.end()
+            }
+        })
+    }
 })
+
 const PORT = 5000
 server.listen(PORT)
 
